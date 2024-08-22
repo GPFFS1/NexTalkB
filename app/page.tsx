d0a5adb2c599cf563899220b03b3e383d5d181d7
GPFFS1 <rafeyodrani1234@gmail.com>
2024-08-22 18:38:53 +0200

    intial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,71 @@
+import Test from "@/components/Test";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-[url('/images/nasa.png')] h-screen flex justify-center items-center">
+      <div className="flex flex-row">
+        <div className="h-[750px] w-[350px] flex items-center flex-col justify-around bg-white/5 rounded-tl-[15px] rounded-bl-[15px] border border-white/50 backdrop-blur-[15px]">
+          <h1 className="text-[40px] font-bold tracking-widest flex">
+            Sign In
+          </h1>
+          <Image
+            alt=""
+            src={"/images/illustration.png"}
+            className="w-[308px] h-[260px]"
+            width={308}
+            height={260}
+          />
+          <div className="flex flex-col gap-8 items-center">
+            <Link
+              href="/signup"
+              className="text-white text-xl font-normal tracking-wide "
+            >
+              Don't have an account?
+            </Link>
+            <button className="border rounded-3xl w-[122px] h-[50px]">
+              Sign Up
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="h-[750px] w-[600px] flex flex-col items-center justify-around bg-white/5 rounded-tr-[15px] rounded-br-[15px] border border-white/50 backdrop-blur-[15px]">
+          <h1 className="text-[40px] font-bold tracking-widest">Log in here</h1>
+          <div className="flex flex-col">
+            <label
+              htmlFor="emailorusername"
+              className="text-white/75 text-xs font-normal tracking-wide"
+            >
+              Username or Email Address*
+            </label>
+            <input
+              type="emailorusername"
+              name="emailorusername"
+              id="emailorusername"
+              className="w-[299px] h-[50px] border border-white/50 rounded-lg bg-transparent"
+            />
+            <label
+              htmlFor="password"
+              className="text-white/75 text-xs font-normal tracking-wide mt-5"
+            >
+              Password*
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="w-[299px] h-[50px] border border-white/50 rounded-lg bg-transparent"
+            />
+          </div>
+          <button className="border rounded-3xl w-[299px] h-[50px] bg-white text-black text-xl font-normal tracking-wide">Log In</button>
+          <button>
+            
+          </button>
+        </div>
       </div>
     </main>
   );
-}
+};
+
+export default HomePage;
---
+++ components/Test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Test = ({ children}:{children:any}) => {
+  return <div>
+    {children}
+    "&nbsp;"
+  </div>;
+};
+
+export default Test;
